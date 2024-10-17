bcd376b992d1dc15e276cf81428ea7694f898547
H.P.Pavithri Apeksha <apeksha-cs20070@stu.kln.ac.lk>
2024-10-17 18:09:01 +0530

    Add volume bar

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -4,9 +4,14 @@ import numpy as np
 import HandTrackingModule as htm
 import math
 import alsaaudio
-import os
+
 mixer = alsaaudio.Mixer()
 vol_range = mixer.getvolume()
+# minVol = vol_range[0]
+# maxVol = vol_range[1]
+vol = 0
+volBar = 400
+volPer = 0
 print(vol_range)
 
 
@@ -33,19 +38,26 @@ while True:
         x2, y2 = lmsList[8][1], lmsList[8][2]
         cx,cy = (x1+x2)//2, (y1+y2)//2
 
-        cv2.circle(img,(x1,y1),10,(255,0,0),cv2.FILLED)
-        cv2.circle(img, (x2, y2), 10, (255, 0, 0), cv2.FILLED)
+        cv2.circle(img,(x1,y1),10,(255,0,255),cv2.FILLED)
+        cv2.circle(img, (x2, y2), 10, (255,0,255), cv2.FILLED)
         cv2.line(img,(x1,y1),(x2,y2),(255,0,255),3)
         cv2.circle(img, (cx, cy), 10, (255, 0, 0), cv2.FILLED)
 
         length = math.hypot(x2-x1,y2-y1)
         #print(length)
 
-        vol = np.interp(length, [50, 250], [0, 100])
+        vol = np.interp(length, [50, 180], [0, 100])
+        volBar = np.interp(length, [50, 180], [400, 150])
+        volPer = np.interp(length, [50, 180], [0, 100])
         mixer.setvolume(int(vol))
+        cv2.putText(img,f' {int(volPer)} %',(40,450),cv2.FONT_HERSHEY_COMPLEX,
+                    1,(255,0,0),3)
 
         if length < 50 :
             cv2.circle(img, (cx, cy), 10, (0, 255, 0), cv2.FILLED)
+
+        cv2.rectangle(img,(50,150),(80,400),(255,0,0),3)
+        cv2.rectangle(img,(50,int(volBar)),(80,400),(255,0,0),cv2.FILLED)
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
